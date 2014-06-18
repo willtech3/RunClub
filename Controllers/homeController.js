ae5c855d87d6b99c8cdd4205fbd7e303139be960
Will Lane <william.west.lane@gmail.com>
2014-06-18 09:03:26 -0400

    Added angular-google-maps

--- Controllers/homeController.js
+++ Controllers/homeController.js
@@ -8,4 +8,16 @@ runClub.controller('homeController',['$scope', function($scope){
         "twoGuys.jpg",
         "on-tap.jpg"
     ];
+
+    $scope.map = {
+        center: {
+            latitude:26.283870,
+            longitude:-80.188257
+        },
+        zoom: 17,
+        options: {
+            scrollwheel: false
+        }
+    };
+
 }]);
